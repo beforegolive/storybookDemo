50b3aacab69fcb31e40af3f1728d989ed8d21e7b
twomeetings1 <hhxu@thoughtworks.com>
2020-09-03 05:23:05 +0800

    add button testcase

---
+++ stories/NormalButton.stories.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Button, Slider as SL } from 'antd'
+
+export default {
+  title:'Button',
+  component: Button
+}
+
+export const Primary = () => <Button type="primary">Primary</Button>;
+
+// Primary.storyName = 'I am the primary';
+
+// Primary.storyName ='I am the primary'
